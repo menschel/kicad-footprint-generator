b4c7000f5f41a16bd28c27abd702345d04553a6c
Patrick Menschel <menschel.p@posteo.de>
2018-04-10 19:06:38 +0200

    fixed pin order and positions of fab,silk,package

--- makefootprint.py
+++ makefootprint.py
@@ -5,6 +5,19 @@
 #import re
 #import math
 
+import matplotlib.pyplot as plt
+def plot_points(points,figname):
+    pts = points.copy()
+    pts.append(points[0])
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    line, = ax.plot([pt[0] for pt in pts],[pt[1] for pt in pts],lw=2,marker='o')
+    for i in range(len(pts)-1):
+        ax.annotate("PT{0} ({1},{2})".format(i,pts[i][0],pts[i][1]),xy=pts[i])
+    ax.set_title(figname)    
+    plt.show()
+    return
+
 def format_pad(padnum,padtype,padshape,posx,posy,sizex,sizey,layers):
     ret = "(pad {0} {1} {2} (at {3} {4}) (size {5} {6}) (layers {7}))".format(padnum, padtype, padshape,posx,posy,sizex,sizey," ".join(layers) )
     return ret
@@ -33,6 +46,14 @@ def get_outer_dimensions_of_pads(pads):
         maxxy = list(max(maxxy[i],padmaxxy[i]) for i in range(2))
     return minxy,maxxy
 
+def get_points_from_dimensions(dims):
+    minxy,maxxy = dims
+    pts = []
+    pts.append((minxy[0],minxy[1]))
+    pts.append((minxy[0],maxxy[1]))
+    pts.append((maxxy[0],maxxy[1]))
+    pts.append((maxxy[0],minxy[1]))
+    return pts
 
 def get_center_dimensions_of_pads(pads):
     minxy = [0,0]
@@ -47,7 +68,23 @@ def format_courtyard_lines(pads,package_dimensions,package_offset=0.25,linewidth
     points = []
     courtyard_lines = []
     grid_decimals = 2
-    for point in get_package_points(package_dimensions):
+    outer_pad_points = get_points_from_dimensions(get_outer_dimensions_of_pads(pads))
+#     plot_points(outer_pad_points,"outer_pad_points")
+    package_points = get_package_points(package_dimensions)
+    
+    outerpoints = []
+    for i in range(4):
+        thispt = [0,0]
+        for j in range(2):
+            if abs(outer_pad_points[i][j]) > abs(package_points[i][j]):#we know the points are aranged in the same manner/sector, so we check abs()
+                thispt[j] = outer_pad_points[i][j]
+#                 print("Pads win at Point {0} dim {1}".format(i,j))
+            else:
+                thispt[j] = package_points[i][j]
+#                 print("Package wins at Point {0} dim {1}".format(i,j))
+        outerpoints.append(thispt)
+            
+    for point in outerpoints:
         x,y = point
         if x < 0:
             thisx = round(x-package_offset-(grid/2),grid_decimals)
@@ -60,6 +97,10 @@ def format_courtyard_lines(pads,package_dimensions,package_offset=0.25,linewidth
             
         points.append((thisx,thisy))
     
+#     plot_points(package_points,"package_points")
+#     
+#     plot_points(points,"courtard after rounding")
+#     
     for idx in range(len(points)-1):
         startpoint,endpoint = points[idx:idx+2]
         courtyard_lines.append(format_fpline(startpoint,endpoint,"F.CrtYd",linewidth))
@@ -74,8 +115,10 @@ def get_package_points(package_dimensions):
     for x in [-0.5,0.5]:
         for y in [-0.5,0.5]:
             points.append((x*package_dimensions[0],y*package_dimensions[1]))
-            
-    return points 
+    rearrangedpoints = points.copy()
+    rearrangedpoints[3] = points[2]
+    rearrangedpoints[2] = points[3]
+    return rearrangedpoints
 
 def format_fab_lines(package_dimensions,linewidth=0.1):
     """ return the lines (as in newline ) for the fabrication layer (geometrical) lines
@@ -90,16 +133,10 @@ def format_fab_lines(package_dimensions,linewidth=0.1):
     bevel = max(0.1,min(package_dimensions)*0.25)
     
     #alter first and last point to bevel
-    points.append((points[0][0],points[0][1]+bevel))
-    points[0] = (points[0][0]+bevel,points[0][1])
+    points.append((points[0][0]+bevel,points[0][1]))
+    points[0] = (points[0][0],points[0][1]+bevel)
     
-    #we have to swap points for this to draw correct
-    newpoints = points.copy()
-    newpoints[1] = points[2]
-    newpoints[2] = points[3]
-    newpoints[3] = points[1]
-    points = newpoints
-    
+#     plot_points(points,"fab lines")
     for idx in range(len(points)-1):
         startpoint,endpoint = points[idx:idx+2]
         fab_lines.append(format_fpline(startpoint,endpoint,"F.Fab",linewidth))
@@ -117,13 +154,16 @@ def format_silks_lines(pads,package_dimensions,distance_to_fablines=0.12,distanc
     faby = [p[1] for p in get_package_points(package_dimensions)]
     fabminy = min(faby) 
     fabmaxy = max(faby)
+#     print(minxy,fabminy,fabmaxy)
         
-    startpoint = minxy[0]+(linewidth/2),min(minxy[1]-distance_to_pads,-fabminy-distance_to_fablines)
-    endpoint = cmaxxy[0],min(minxy[1]-distance_to_pads,-fabminy-distance_to_fablines)
+    startpoint = minxy[0]+(linewidth/2),min(minxy[1]-distance_to_pads,fabminy-distance_to_fablines)
+    endpoint = cmaxxy[0],min(minxy[1]-distance_to_pads,fabminy-distance_to_fablines)
+#     print(startpoint,endpoint)
     silks_lines.append(format_fpline(startpoint,endpoint,"F.SilkS",linewidth))
     
     startpoint = cminxy[0],max(maxxy[1]+distance_to_pads,fabmaxy+distance_to_fablines)
-    endpoint = cmaxxy[0],maxxy[1]+max(distance_to_pads,fabmaxy+distance_to_fablines)
+    endpoint = cmaxxy[0],max(maxxy[1]+distance_to_pads,fabmaxy+distance_to_fablines)
+#     print(startpoint,endpoint)
     silks_lines.append(format_fpline(startpoint,endpoint,"F.SilkS",linewidth))
     return silks_lines
 
@@ -225,7 +265,7 @@ class footprint_pad():
 class kicad_footprint:
     
      
-    def __init__(self,name,desc,datasheet,pads,tedit="5AA01C76",layers=["F.Cu",],tags=["VDN",],attr=["smd",],model3dname="Package_DFN_QFN.3dshapes/DFN-14-1EP_3x4.5mm_P0.65mm.wrl",package_dimensions=(1,1)):
+    def __init__(self,name,desc,datasheet,pads,tedit="5AA01C76",layers=["F.Cu",],tags=["TDFN",],attr=["smd",],model3dname="Package_DFN_QFN.3dshapes/DFN-14-1EP_3x4.5mm_P0.65mm.wrl",package_dimensions=(1,1)):
         self.name = name
         self.desc = desc
         self.datasheet = datasheet
@@ -303,8 +343,12 @@ def make_footprint_stmicro(N,E,X2,Y2,C,X,Y,V,EV,modulename,description,datasheet
     pads = []
     thermalvia_pads = []
     paste_pads = []    
-    for padnum,xypos in enumerate(get_posxy_for_span(pinnum=N,spanx=E,spany=C)):        
-        pads.append(footprint_pad(padnum+1,
+    for idx,xypos in enumerate(get_posxy_for_span(pinnum=N,spanx=E,spany=C)):
+        if idx >= N/2:
+            padnum = N - idx + int(N/2) 
+        else:
+            padnum=idx+1        
+        pads.append(footprint_pad(padnum,
                                   xypos=(xypos[1],xypos[0]),
                                   sizexy=(Y,X),
                                   padtype="smd", padshape="oval",layers = ["F.Cu","F.Paste","F.Mask"]))
@@ -364,7 +408,7 @@ def make_footprint_stmicro(N,E,X2,Y2,C,X,Y,V,EV,modulename,description,datasheet
         pads.extend(thermalvia_pads)
     pads.extend(paste_pads)    
     
-    fp_obj = kicad_footprint(name=modulename,desc=description,datasheet=datasheet,pads=pads,tags=["VDFN","DFN","{0}mm".format(E)],
+    fp_obj = kicad_footprint(name=modulename,desc=description,datasheet=datasheet,pads=pads,tags=["TDFN","DFN","{0}mm".format(E)],
                              model3dname="Package_DFN_QFN.3dshapes/{0}.wrl".format(modulename.replace("_ThermalVias","")),
                              package_dimensions=package_dimensions)    
     return fp_obj.format()
@@ -391,6 +435,26 @@ if __name__ == "__main__":
     with open("{0}.kicad_mod".format(modulename),"w") as f:
         f.write(footprint)
 
+    modulename = "TDFN-8-1EP_3x2mm_P0.5mm_EP1.80x1.65mm"
+    footprint = make_footprint_stmicro(N=8,
+                                       E=0.5,
+                                       X2=1.65,
+                                       Y2=1.8,
+                                       C=2.9,
+                                       X=0.25,
+                                       Y=0.85,
+                                       V=0.3,
+                                       EV=1.0,
+                                       modulename=modulename,
+                                       description="8-lead plastic dual flat, 2x3x0.75mm size, 0.5mm pitch",
+                                       datasheet="http://ww1.microchip.com/downloads/en/DeviceDoc/8L_TDFN_2x3_MN_C04-0129E-MN.pdf",
+                                       centerpad=True,
+                                       numthermalvias=4,
+                                       package_dimensions=(3,2),
+                                       generate_thermalvias=False,
+                                       )
+    with open("{0}.kicad_mod".format(modulename),"w") as f:
+        f.write(footprint)
 
 
 
